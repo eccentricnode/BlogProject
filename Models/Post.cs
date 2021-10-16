29b3828b406350db61d2c71dd47d3dc7a9009698
Austin Johnson <austin.b.johnson98@gmail.com>
2021-10-15 23:36:02 -0700

    Add project files.

---
+++ Models/Post.cs
@@ -0,0 +1,60 @@
+﻿using BlogProject.Enums;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Blog Name")]
+        public int BlogId { get; set; }
+
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(200, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        public string Abstract { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
+        public DateTime Created { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
+        public DateTime? Updated { get; set; }
+
+        public ReadyStatus ReadyStatus { get; set; }
+
+        public string Slug { get; set; }
+
+        public byte[] ImageData { get; set; }
+        public string ContentType { get; set; }
+
+        [NotMapped]
+        public IFormFile Image { get; set; }
+
+        // Navigation Properties
+        // Child of Blog, so a reference to send collection to Blog (via this property and Author property also.).
+        public virtual Blog Blog { get; set; }
+        public virtual BlogUser Author { get; set; }
+        
+        // Navigation Property that is the parent... Or it collects the Tags, in this case.
+        public virtual ICollection<Tag> Tags { get; set; } = new HashSet<Tag>();
+        public virtual ICollection<Comment> Comments { get; set; } = new HashSet<Comment>();
+    }
+}
