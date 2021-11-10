e89e13c11e80c46d02d2a503ad40eedeacf8c962
Austin Johnson <austin.b.johnson98@gmail.com>
2021-11-10 15:19:27 -0700

    created and implemented logic for a slug service.

--- Startup.cs
+++ Startup.cs
@@ -56,6 +56,9 @@ namespace BlogProject
 
             // register image service
             services.AddScoped<IImageService, BasicImageService>();
+
+            // register slug service
+            services.AddScoped<ISlugService, BasicSlugService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
